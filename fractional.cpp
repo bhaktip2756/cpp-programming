dec64701d2d5b2a96f9d36862327ef50784bde47
bhaktip2756 <bhaktip2756@gmail.com>
2026-08-19 05:07:30 +0000

    change

--- fractional.cpp
+++ fractional.cpp
@@ -1,7 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-Class Number{
-    public:
-    int 
+class Fraction
+{
+    int numerator, denominator;
+
+public:
+    void getData()
+    {
+        cout << "Enter numerator: ";
+        cin >> numerator;
+
+        cout << "Enter denominator: ";
+        cin >> denominator;
+    }
+
+    Fraction add(Fraction f)
+    {
+        Fraction result;
+
+        result.numerator = (numerator * f.denominator) +
+                           (f.numerator * denominator);
+
+        result.denominator = denominator * f.denominator;
+
+        return result;
+    }
+
+    void display()
+    {
+        cout << numerator << "/" << denominator;
+    }
+};
+
+int main()
+{
+    Fraction f1, f2, sum;
+
+    cout << "Enter first fraction:\n";
+    f1.getData();
+
+    cout << "\nEnter second fraction:\n";
+    f2.getData();
+
+    sum = f1.add(f2);
+
+    cout << "\nSum of fractions = ";
+    sum.display();
+
+    return 0;
 }
